78994e83e0ccbdd90b9cf0a0ad7470f5b80954e9
lpw970779687 <970779687@qq.com>
2026-04-21 12:41:58 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,66 @@
+const shuihuConfig = {
+    characters: [
+        {
+            name: "宋江",
+            title: "及时雨",
+            description: "梁山泊首领，呼保义宋江，一百零八将之首",
+            imagePath: "images/水浒传/人物/宋江/",
+            cover: "001-宋江-1.jpg",
+            images: [
+                "001-宋江-1.jpg",
+                "001-宋江-2-wukong.jpg",
+                "001-宋江-3-wukong.jpg",
+                "001-宋江-4-豆包.jpg",
+                "001-宋江-5-豆包.jpg",
+                "001-宋江-6-豆包.jpg",
+                "001-宋江-7-gmini.jpg",
+                "001-宋江-8-gmini.jpg"
+            ]
+        },
+        {
+            name: "卢俊义",
+            title: "玉麒麟",
+            description: "北京大名府富商，武艺高强，棍棒天下无对",
+            imagePath: "images/水浒传/人物/卢俊义/",
+            cover: "002-卢俊义-1.jpg",
+            images: [
+                "002-卢俊义-1.jpg",
+                "002-卢俊义-2-wukong.jpg",
+                "002-卢俊义-3-wukong.jpg",
+                "002-卢俊义-4-豆包.jpg",
+                "002-卢俊义-5-豆包.jpg",
+                "002-卢俊义-6-gmin.jpg",
+                "002-卢俊义-7-gmin.jpg"
+            ]
+        }
+    ]
+};
+
+const xiyouConfig = {
+    characters: [
+        {
+            name: "唐三藏",
+            title: "唐僧",
+            description: "金蝉子转世，西天取经的领导者，慈悲为怀",
+            imagePath: "images/西游记/人物/唐三藏/",
+            cover: "001-唐三藏-1.jpg",
+            images: [
+                "001-唐三藏-1.jpg",
+                "001-唐三藏-2-豆包.jpg",
+                "001-唐三藏-3-wukong.jpg"
+            ]
+        },
+        {
+            name: "孙悟空",
+            title: "孙悟空",
+            description: "石头中出来的孙悟空，有超凡的武艺，有超凡的智慧",
+            imagePath: "images/西游记/人物/孙悟空/",
+            cover: "001-孙悟空-1.jpg",
+            images: [
+                "001-孙悟空-1.jpg",
+                "001-孙悟空-2-豆包.jpg",
+                "001-孙悟空-3-wukong.jpg"
+            ]
+        },
+    ]
+};
