2e4c71671e5a99ed5ac167a23eb78f638bbce084
luopengwei <453989504@qq.com>
2026-04-26 22:00:47 +0800

    1、增加水浒人物

--- config.js
+++ config.js
@@ -2,38 +2,249 @@ const shuihuConfig = {
     characters: [
         {
             name: "宋江",
-            title: "及时雨",
+            title: "天魁星-及时雨",
             description: "梁山泊首领，呼保义宋江，一百零八将之首",
-            imagePath: "images/水浒传/人物/宋江/",
-            cover: "001-宋江-1.jpg",
+            biography: "本为郓城县押司，为人仗义疏财、体恤弱者，常帮扶江湖好汉，故而名声远扬。因杀死阎婆惜亡命江湖，辗转投奔梁山。晁盖身故后，继任梁山寨主，整肃山寨、收拢群雄，主导梁山发展。一心效忠朝廷，主张招安，率领梁山兵马北征辽国、南讨叛寇，屡立战功。平定方腊之乱后，遭奸臣毒杀，一生忠义却结局悲凉。",
+            imagePath: "images/水浒传/人物/001-宋江/",
+            cover: "1.jpg",
             images: [
-                "001-宋江-1.jpg",
-                "001-宋江-2-wukong.jpg",
-                "001-宋江-3-wukong.jpg",
-                "001-宋江-4-豆包.jpg",
-                "001-宋江-5-豆包.jpg",
-                "001-宋江-6-豆包.jpg",
-                "001-宋江-7-gmini.jpg",
-                "001-宋江-8-gmini.jpg"
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg",
+                "8.jpg"
             ],
             videos: ["宋江.mp4"]
         },
         {
             name: "卢俊义",
-            title: "玉麒麟",
+            title: "天罡星-玉麒麟",
             description: "北京大名府富商，武艺高强，棍棒天下无对",
-            imagePath: "images/水浒传/人物/卢俊义/",
-            cover: "002-卢俊义-1.jpg",
+            imagePath: "images/水浒传/人物/002-卢俊义/",
+            cover: "1.jpg",
             images: [
-                "002-卢俊义-1.jpg",
-                "002-卢俊义-2-wukong.jpg",
-                "002-卢俊义-3-wukong.jpg",
-                "002-卢俊义-4-豆包.jpg",
-                "002-卢俊义-5-豆包.jpg",
-                "002-卢俊义-6-gmin.jpg",
-                "002-卢俊义-7-gmin.jpg"
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg"
             ],
-            videos: []
+            videos: ["卢俊义.mp4"]
+        },
+        {
+            name: "吴用",
+            title: "天机星-智多星",
+            description: "梁山泊首领，呼保义吴用，一百零八将之首",
+            imagePath: "images/水浒传/人物/003-吴用/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg",
+                "8.jpg"
+            ],
+            videos: ["吴用.mp4"]
+        },
+        {
+            name: "公孙胜",
+            title: "天闲星-入云龙",
+            description: "晁盖旧部，道术通天，呼风唤雨；征辽平王庆后归隐二仙山",
+            imagePath: "images/水浒传/人物/004-公孙胜/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg",
+                "8.jpg"
+            ],
+            videos: ["公孙胜.mp4"]
+        },
+        {
+            name: "关胜",
+            title: "天勇星-大刀",
+            description: "---",
+            imagePath: "images/水浒传/人物/005-关胜/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg",
+                "8.jpg",
+                "9.jpg"
+            ],
+            videos: ["关胜.mp4"]
+        },
+        {
+            name: "林冲",
+            title: "天雄星-豹子头",
+            description: "---",
+            imagePath: "images/水浒传/人物/006-林冲/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg",
+                "8.jpg",
+                "9.jpg"
+            ],
+            videos: ["林冲.mp4"]
+        },
+        {
+            name: "秦明",
+            title: "天猛星-霹雳火",
+            description: "---",
+            imagePath: "images/水浒传/人物/007-秦明/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg"
+            ],
+            videos: ["秦明.mp4"]
+        },
+        {
+            name: "呼延灼",
+            title: "天威星-双鞭",
+            description: "---",
+            imagePath: "images/水浒传/人物/008-呼延灼/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+            ],
+            videos: ["呼延灼.mp4"]
+        },
+        {
+            name: "花荣",
+            title: "天英星-小李广",
+            description: "---",
+            imagePath: "images/水浒传/人物/009-花荣/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg"
+            ],
+            videos: ["花荣.mp4"]
+        },
+        {
+            name: "柴进",
+            title: "天贵星-小旋风",
+            description: "---",
+            imagePath: "images/水浒传/人物/010-柴进/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg",
+                "8.jpg"
+            ],
+            videos: ["柴进.mp4"]
+        },
+        {
+            name: "李应",
+            title: "天富星-扑天雕",
+            description: "---",
+            imagePath: "images/水浒传/人物/011-李应/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg"
+            ],
+            videos: ["李应.mp4"]
+        },
+        {
+            name: "朱仝",
+            title: "天满星-美髯公",
+            description: "---",
+            imagePath: "images/水浒传/人物/012-朱仝/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg"
+            ],
+            videos: ["朱仝.mp4"]
+        },
+        {
+            name: "鲁智深",
+            title: "天孤星-花和尚",
+            description: "---",
+            imagePath: "images/水浒传/人物/013-鲁智深/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg"
+            ],
+            videos: ["鲁智深.mp4"]
+        },
+        {
+            name: "武松",
+            title: "天伤星-行者",
+            description: "---",
+            imagePath: "images/水浒传/人物/014-武松/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg",
+                "5.jpg",
+                "6.jpg",
+                "7.jpg"
+            ],
+            videos: ["武松.mp4"]
+        },
+        {
+            name: "董平",
+            title: "天立星-双枪将",
+            description: "---",
+            imagePath: "images/水浒传/人物/015-董平/",
+            cover: "1.jpg",
+            images: [
+                "1.jpg",
+                "2.jpg",
+                "3.jpg",
+                "4.jpg"
+            ],
+            videos: ["董平.mp4"]
         }
     ]
 };
